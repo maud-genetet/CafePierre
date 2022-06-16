3e465524f376b8e59c8fd2a3e7eebe89c5f8e7cf
maud-genetet <maud.genetet@gmail.com>
2022-06-16 17:57:27 +0200

    mise a jour site avec nouveaux graphes

--- js/grapheEmpreinte.js
+++ js/grapheEmpreinte.js
@@ -1,8 +1,16 @@
 const select = document.querySelector('#pays-select');
+const select2 = document.querySelector('#pays2-select');
 const img1 = document.querySelector('#img-1');
+const img2 = document.querySelector('#img-2');
 
 select.addEventListener('change', function(e) {
     const pays = e.target.value;
     img1.src = `plot/GHG'${pays}'.png`;
+}
+);
+
+select2.addEventListener('change', function(e) {
+    const pays = e.target.value;
+    img2.src = `plot/ProductionOfGHGPourcentage'${pays}'.png`;
 }
 );
--- js/grapheEnergie.js
+++ js/grapheEnergie.js
@@ -1,9 +1,11 @@
 const select = document.querySelector('#pays-select');
 const select2 = document.querySelector('#pays2-select');
 const select3 = document.querySelector('#secteur-select');
+const select4 = document.querySelector('#pays4-select');
 const img1 = document.querySelector('#img-1');
 const img2 = document.querySelector('#img-2');
 const img3 = document.querySelector('#img-3');
+const img4 = document.querySelector('#img-4');
 
 
 select.addEventListener('change', function(e) {
@@ -21,5 +23,11 @@ select2.addEventListener('change', function(e) {
 select3.addEventListener('change', function(e) {
     const secteur = e.target.value;
     img3.src = `plot/ProductionOfEnergyWithTheSource${secteur}.png`;
+}
+);
+
+select4.addEventListener('change', function(e) {
+    const pays = e.target.value;
+    img4.src = `plot/ProductionVSConsumptionPrimaryEnergy.'${pays}'.png`;
 }
 );
